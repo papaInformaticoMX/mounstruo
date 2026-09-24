0283953dae6aa892d5c5ee7374d2f0dd5904f1a0
Pepe Rosado <peperosadoimss@gmail.com>
2026-09-24 10:35:05 -0600

    remove code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { GameComponent } from './game/game.component';
 
 @Component({
   imports: [ GameComponent],
   selector: 'app-root',
-  styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('mounstruo');
 }
